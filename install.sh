08f0b50850c458a3358755fba50dac7ace8eabd6
Bilibili_BHznJNs <67158072+BHznJNs@users.noreply.github.com>
2021-05-29 17:51:14 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+# 如果文件夹不存在，克隆储存库
+if [ ! -d "./NeteaseCloudMusicApi" ]; then
+  git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git
+fi
+cd NeteaseCloudMusicApi
+# 安装 express
+if [ ! -d "./node_modules/express" ]; then
+  npm install express -s
+  # 文件重命名
+  mv app.js NCMapi.js
+fi
+
+cd ../
+# 安装依赖库
+npm install
+# 如果 Electron 未安装成功
+if [ ! -d "./node_modules/electron/dist" ]; then
+  node ./node_modules/electron/install.js
+fi
+
+# npm start
